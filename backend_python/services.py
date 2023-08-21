0deec008042882098adda5c2507084e67f1a7727
Roope <2.r.k.niemi@gmail.com>
2023-08-21 18:10:47 +0300

    r

--- backend_python/services.py
+++ backend_python/services.py
@@ -19,11 +19,6 @@ class SignUpRequest(BaseModel):
     password: str
 
 
-class SolutionRequest(BaseModel):
-    board: list[list[int]]
-    blocks: list[str]
-
-
 class Token(BaseModel):
     access_token: str
     token_type: str
