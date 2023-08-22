acfb64632945527eca93c5cecb1eff4326d80c4e
Roope <2.r.k.niemi@gmail.com>
2023-08-22 11:19:13 +0300

    test

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,9 +1,9 @@
 import Constants from "expo-constants";
 import { io } from "socket.io-client";
 
-const baseUrl = `${Constants.manifest.extra.ws}`;
+const baseUrl = `${Constants.manifest.extra.uri}`;
 
 export const socket = io(baseUrl, {
-  path: "/ws/socket.io",
   transports: ["websocket"],
+  secure: true,
 });
